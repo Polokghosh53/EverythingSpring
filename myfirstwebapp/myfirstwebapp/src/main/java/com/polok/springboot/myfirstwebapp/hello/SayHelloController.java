812483f5fb26f0a697ba016055ed35f14f01dd40
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2023-04-03 00:16:48 +0530

    Add files via upload

---
+++ myfirstwebapp/myfirstwebapp/src/main/java/com/polok/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -0,0 +1,20 @@
+package com.polok.springboot.myfirstwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+	
+	//"say-hello" => "Hello! What are you learning today?"
+	
+	//say-hello
+	// http://localhost:8080/say-hello
+	@RequestMapping("say-hello")
+	@ResponseBody
+	public String sayHello() {
+		return "Hello! What are you learning today?";
+	}
+	
+}
