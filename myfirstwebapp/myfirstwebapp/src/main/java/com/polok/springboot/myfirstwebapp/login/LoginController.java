f5ed29bcbb127e76e3ebb432eb3786adf87286bc
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2023-04-12 23:34:27 +0530

    Update LoginController.java

--- myfirstwebapp/myfirstwebapp/src/main/java/com/polok/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/myfirstwebapp/src/main/java/com/polok/springboot/myfirstwebapp/login/LoginController.java
@@ -1,16 +1,22 @@
 package com.polok.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 //	/login => com.polok.springboot.myfirstwebapp.login.LoginController => login.jsp
 	
+	// http://localhost:8080/login?name=Ranga
+	// Model
 	@RequestMapping("login")
 //	@ResponseBody
-	public String gotoLoginPage() {
+	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
+		model.put("name", name);
+		System.out.println("Request param is " + name); // NOT RECOMMENDED FOR PROD CODE
 		return "login";
 	}
 }
