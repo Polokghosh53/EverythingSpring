3d6717f89088376e2e996fffb61d0b1ab99f1e3f
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2023-04-13 23:50:45 +0530

    Update LoginController.java

--- myfirstwebapp/myfirstwebapp/src/main/java/com/polok/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/myfirstwebapp/src/main/java/com/polok/springboot/myfirstwebapp/login/LoginController.java
@@ -1,13 +1,16 @@
 package com.polok.springboot.myfirstwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
+	
+//	private Logger logger = LoggerFactory.getLogger(getClass());
 //	/login => com.polok.springboot.myfirstwebapp.login.LoginController => login.jsp
 	
 	// http://localhost:8080/login?name=Ranga
@@ -16,6 +19,10 @@ public class LoginController {
 //	@ResponseBody
 	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
 		model.put("name", name);
+//		logger.debug("Request param is {}",name);
+//		logger.info("I want this printed at info level");
+//		logger.warn("I want this printed at warn level");
+		
 		System.out.println("Request param is " + name); // NOT RECOMMENDED FOR PROD CODE
 		return "login";
 	}
