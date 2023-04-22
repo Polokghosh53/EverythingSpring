a0a8cf0d70d8c60ec6e11fafaa4ba0db867d2329
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2023-04-22 23:54:56 +0530

    Add files via upload

--- myfirstwebapp/myfirstwebapp/src/main/java/com/polok/springboot/myfirstwebapp/todo/TodoController.java
+++ myfirstwebapp/myfirstwebapp/src/main/java/com/polok/springboot/myfirstwebapp/todo/TodoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -25,5 +26,16 @@ public class TodoController {
 		model.addAttribute("todos",todos);
 		
 		return "listTodos";
+	}
+	
+	// GET, POST
+	@RequestMapping(value="add-todo", method=RequestMethod.GET) 
+	public String showNewTodoPage() {
+		return "todo";
+	}
+	
+	@RequestMapping(value="add-todo", method=RequestMethod.POST) 
+	public String addNewTodo() {
+		return "redirect:list-todos";
 	}
 }
