b7203d0c147e51556d64e93cfe40c5aea9fa692c
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2023-04-23 23:32:18 +0530

    Update TodoController.java

--- myfirstwebapp/myfirstwebapp/src/main/java/com/polok/springboot/myfirstwebapp/todo/TodoController.java
+++ myfirstwebapp/myfirstwebapp/src/main/java/com/polok/springboot/myfirstwebapp/todo/TodoController.java
@@ -1,11 +1,13 @@
 package com.polok.springboot.myfirstwebapp.todo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -35,7 +37,9 @@ public class TodoController {
 	}
 	
 	@RequestMapping(value="add-todo", method=RequestMethod.POST) 
-	public String addNewTodo() {
+	public String addNewTodo(@RequestParam String description, ModelMap model) {
+		String username = (String) model.get("name");
+		todoService.addTodo(username, description, LocalDate.now().plusYears(1), false);
 		return "redirect:list-todos";
 	}
 }
