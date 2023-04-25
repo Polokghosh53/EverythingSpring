4cd870ce71d69d4c6d200b6fadd16576e4dd309e
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2023-04-25 21:05:03 +0530

    Add files via upload

--- myfirstwebapp/myfirstwebapp/src/main/java/com/polok/springboot/myfirstwebapp/todo/TodoController.java
+++ myfirstwebapp/myfirstwebapp/src/main/java/com/polok/springboot/myfirstwebapp/todo/TodoController.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import jakarta.validation.Valid;
 
 @Controller
 @SessionAttributes("name")
@@ -34,14 +36,18 @@ public class TodoController {
 	@RequestMapping(value="add-todo", method=RequestMethod.GET) 
 	public String showNewTodoPage(ModelMap model) {
 		String username = (String) model.get("name");
-		// adding default value for todo
 		Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
 		model.put("todo",todo);
 		return "todo";
 	}
 	
 	@RequestMapping(value="add-todo", method=RequestMethod.POST) 
-	public String addNewTodo(ModelMap model, Todo todo) {
+	public String addNewTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+		
+		if (result.hasErrors()) {
+			return "todo";
+		}
+		
 		String username = (String) model.get("name");
 		todoService.addTodo(username, todo.getDescription(), LocalDate.now().plusYears(1), false);
 		return "redirect:list-todos";
--- myfirstwebapp/myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/todo.jsp
+++ myfirstwebapp/myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/todo.jsp
@@ -11,6 +11,7 @@
 			<form:form method="post" modelAttribute="todo">
 				Description: <form:input type="text" path="description" 
 				required="required"/>
+				<form:errors path="description" cssClass="text-warning"/>
 				<form:input type="hidden" path="id"/>
 				<form:input type="hidden" path="done"/>
 				<input type="submit" class="btn btn-success">
