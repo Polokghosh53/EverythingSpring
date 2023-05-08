d2490ded490ef9dc8aed3052fe093a4808254bd4
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2023-05-09 00:19:11 +0530

    Update TodoController.java

--- myfirstwebapp/myfirstwebapp/src/main/java/com/polok/springboot/myfirstwebapp/todo/TodoController.java
+++ myfirstwebapp/myfirstwebapp/src/main/java/com/polok/springboot/myfirstwebapp/todo/TodoController.java
@@ -62,10 +62,23 @@ public class TodoController {
 		return "redirect:list-todos";
 	}
 	
-	@RequestMapping("update-todo") 
+	@RequestMapping(value="update-todo", method=RequestMethod.GET) 
 	public String showUpdateTodoPage(@RequestParam int id, ModelMap model) {
 		Todo todo = todoService.findById(id);
 		model.addAttribute("todo",todo);
-		return "redirect:todo";
+		return "todo";
+	}
+	
+	@RequestMapping(value="update-todo", method=RequestMethod.POST) 
+	public String updateTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+		
+		if (result.hasErrors()) {
+			return "todo";
+		}
+		
+		String username = (String) model.get("name");
+		todo.setUsername(username);
+		todoService.updateTodo(todo);
+		return "redirect:list-todos";
 	}
 }
